816351a9fa6456d5cfd78bd6359c3f69343f27e4
Amirhossein Banavi <ahbanavi@gmail.com>
2022-07-24 17:12:04 +0430

    add prettier

--- src/30nama.user.js
+++ src/30nama.user.js
@@ -12,83 +12,86 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    var pushState = history.pushState;
-    var replaceState = history.replaceState;
+  var pushState = history.pushState;
+  var replaceState = history.replaceState;
 
-    history.pushState = function() {
-        pushState.apply(history, arguments);
-        window.dispatchEvent(new Event('pushstate'));
-        window.dispatchEvent(new Event('locationchange'));
-    };
+  history.pushState = function () {
+    pushState.apply(history, arguments);
+    window.dispatchEvent(new Event("pushstate"));
+    window.dispatchEvent(new Event("locationchange"));
+  };
 
-    history.replaceState = function() {
-        replaceState.apply(history, arguments);
-        window.dispatchEvent(new Event('replacestate'));
-        window.dispatchEvent(new Event('locationchange'));
-    };
+  history.replaceState = function () {
+    replaceState.apply(history, arguments);
+    window.dispatchEvent(new Event("replacestate"));
+    window.dispatchEvent(new Event("locationchange"));
+  };
 
-    window.addEventListener('popstate', function() {
-        window.dispatchEvent(new Event('locationchange'))
-    });
+  window.addEventListener("popstate", function () {
+    window.dispatchEvent(new Event("locationchange"));
+  });
 
-
-    window.addEventListener('locationchange', function(){
-        urlChanged();
-    })
-
-    function urlChanged(){
-        var url = window.location.href
-
-        // call dlorder function if url contains ?section=download
-        if(url.indexOf('?section=download') > -1){
-            setTimeout(dlorder, 500);
-        }
-    };
-
+  window.addEventListener("locationchange", function () {
     urlChanged();
+  });
 
-    function dlorder(){
-        console.log('30nama Custom DL Order Loaded!');
-        // wait untill all divs with skeleton-bar class are disappeared
-        function w8forSkeletonBarRemoved() {
-            var targetNodes = document.querySelectorAll('div.skeleton-bar');
-            if (targetNodes.length > 0) {
-                setTimeout(w8forSkeletonBarRemoved, 100);
-            } else {
-                actionFunction();
-            }
-        }
+  function urlChanged() {
+    var url = window.location.href;
 
-        w8forSkeletonBarRemoved();
-
-        function actionFunction(){
-            // query all div with class="download-section-main"
-            var downloadSectionMain = document.querySelectorAll('section.download-section-main');
-
-            // reverse the order of items in main download section
-            for (var i = 0; i < downloadSectionMain.length; i++) {
-                var downloadSectionMainItem = downloadSectionMain[i];
-                downloadSectionMainItem.parentNode.insertBefore(downloadSectionMainItem, downloadSectionMainItem.parentNode.firstChild);
-            }
-
-            // query div class="long-info-bar-wrapper" ul li p with value PSA
-            var all_blocks = document.querySelectorAll('div.long-info-bar-wrapper ul li p');
-            // reverse the list
-            all_blocks = Array.prototype.slice.call(all_blocks).reverse();
-
-            var psa_count = 0;
-            for (i = 0; i < all_blocks.length; i++) {
-                if (all_blocks[i].innerHTML.indexOf('PSA') > -1) {
-                    psa_count++;
-                    // move this block to the top of the page
-                    var psa_block = all_blocks[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
-                    var page_block = psa_block.parentNode.parentNode;
-                    page_block.insertBefore(psa_block, page_block.firstChild);
-                }
-            }
-        }
+    // call dlorder function if url contains ?section=download
+    if (url.indexOf("?section=download") > -1) {
+      setTimeout(dlorder, 500);
     }
+  }
+
+  urlChanged();
+
+  function dlorder() {
+    console.log("30nama Custom DL Order Loaded!");
+    // wait untill all divs with skeleton-bar class are disappeared
+    function w8forSkeletonBarRemoved() {
+      var targetNodes = document.querySelectorAll("div.skeleton-bar");
+      if (targetNodes.length > 0) {
+        setTimeout(w8forSkeletonBarRemoved, 100);
+      } else {
+        actionFunction();
+      }
+    }
+
+    w8forSkeletonBarRemoved();
+
+    function actionFunction() {
+      // query all div with class="download-section-main"
+      var downloadSectionMain = document.querySelectorAll("section.download-section-main");
+
+      // reverse the order of items in main download section
+      for (var i = 0; i < downloadSectionMain.length; i++) {
+        var downloadSectionMainItem = downloadSectionMain[i];
+        downloadSectionMainItem.parentNode.insertBefore(
+          downloadSectionMainItem,
+          downloadSectionMainItem.parentNode.firstChild
+        );
+      }
+
+      // query div class="long-info-bar-wrapper" ul li p with value PSA
+      var all_blocks = document.querySelectorAll("div.long-info-bar-wrapper ul li p");
+      // reverse the list
+      all_blocks = Array.prototype.slice.call(all_blocks).reverse();
+
+      var psa_count = 0;
+      for (i = 0; i < all_blocks.length; i++) {
+        if (all_blocks[i].innerHTML.indexOf("PSA") > -1) {
+          psa_count++;
+          // move this block to the top of the page
+          var psa_block = all_blocks[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+          var page_block = psa_block.parentNode.parentNode;
+
+          page_block.insertBefore(psa_block, page_block.firstChild);
+        }
+      }
+    }
+  }
 })();
